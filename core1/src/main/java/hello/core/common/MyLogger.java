72244df6859779c470def6b1855e41eba2e0e8e4
Laplace <zhjlee11@naver.com>
2023-07-04 15:21:41 +0900

    9강

---
+++ core1/src/main/java/hello/core/common/MyLogger.java
@@ -0,0 +1,37 @@
+package hello.core.common;
+
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
+import org.springframework.stereotype.Component;
+
+import java.util.UUID;
+
+@Component
+@Scope(value="request", proxyMode = ScopedProxyMode.TARGET_CLASS) //가짜 프록시 클래스를 하나 만들어서 넣어준다.
+public class MyLogger {
+    private String uuid;
+    private String requestURL;
+
+
+    public void setRequestURL(String requestURL) {
+        this.requestURL = requestURL;
+    }
+
+    public void log(String message) {
+        System.out.println("[" + uuid + "][" + requestURL + "]" + message);
+    }
+
+    @PostConstruct
+    public void init() {
+        uuid = UUID.randomUUID().toString();
+        System.out.println("[" + uuid + "] request scope bean create:" + this);
+    }
+
+    @PreDestroy
+    public void close() {
+        System.out.println("[" + uuid + "] request scope bean close:" + this);
+    }
+
+}
---
+++ core1/src/test/java/hello/core/scope/PrototypeProviderTest.java
@@ -0,0 +1,65 @@
+package hello.core.scope;
+
+import ch.qos.logback.core.net.server.Client;
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+import jakarta.inject.Provider;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class PrototypeProviderTest {
+    @Test
+    void providerTest() {
+        AnnotationConfigApplicationContext ac = new
+                AnnotationConfigApplicationContext(ClientBean.class, PrototypeBean.class);
+
+        ClientBean clientBean1 = ac.getBean(ClientBean.class);
+        int count1 = clientBean1.logic();
+        assertThat(count1).isEqualTo(1);
+        ClientBean clientBean2 = ac.getBean(ClientBean.class);
+        int count2 = clientBean2.logic();
+        assertThat(count2).isEqualTo(1);
+    }
+
+    @ComponentScan
+    static class ClientBean {
+        @Autowired
+        private Provider<PrototypeBean> provider;
+
+        public int logic() {
+            PrototypeBean prototypeBean = provider.get();
+            prototypeBean.addCount();
+            int count = prototypeBean.getCount();
+            return count;
+        }
+
+    }
+    @Scope("prototype")
+    static class PrototypeBean {
+        private int count = 0;
+        public void addCount() {
+            count++;
+        }
+        public int getCount() {
+            return count;
+        }
+        @PostConstruct
+        public void init() {
+            System.out.println("PrototypeBean.init " + this);
+        }
+        @PreDestroy
+        public void destroy() {
+            System.out.println("PrototypeBean.destroy");
+        }
+    }
+}
